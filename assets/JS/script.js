5734c24a4f7dedb07afb92f7bb991da693e803bc
Lucre <josielindamaldonado@gmail.com>
2022-04-14 00:39:38 -0400

    added rough draft of dialog form.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,5 @@
 // DOM ELEMENTS
-
+var addProjectBtn = $('#add-project-btn');
 
 // VARIABLES
 
@@ -7,7 +7,67 @@
 setInterval(function(){
     var currentDate = moment().format('MMM Do YYYY, h:mm:ss a');
     $('#date-time').text(currentDate);
-    // $('#date-time').text("");
-    console.log(currentDate);
 }, 1000);
 
+addProjectBtn.on('click', function(){
+    console.log('clicked');
+    dialog.dialog('open');
+})
+
+
+
+
+var dialog = $( "#dialog-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+      "Add Project": addEntry,
+      Cancel: function() {
+        dialog.dialog( "close" );
+      }
+    },
+    close: function() {
+      form[ 0 ].reset();
+      allFields.removeClass( "ui-state-error" );
+    }
+  });
+
+
+
+
+  function addEntry() {
+    var valid = true;
+    allFields.removeClass( "ui-state-error" );
+
+    valid = valid && checkLength( name, "username", 3, 16 );
+    valid = valid && checkLength( email, "email", 6, 80 );
+    valid = valid && checkLength( password, "password", 5, 16 );
+
+    valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+    valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+    valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+
+    if ( valid ) {
+      $( "#users tbody" ).append( "<tr>" +
+        "<td>" + name.val() + "</td>" +
+        "<td>" + email.val() + "</td>" +
+        "<td>" + password.val() + "</td>" +
+      "</tr>" );
+      dialog.dialog( "close" );
+    }
+    return valid;
+  }
+
+
+
+
+  var form = dialog.find( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    addUser();
+  });
+
+//   $( "#create-user" ).button().on( "click", function() {
+//     dialog.dialog( "open" );
+//   });
