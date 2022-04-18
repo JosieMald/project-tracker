cc3916838e1f19d2c9093d27ac909085f1356371
Lucre <josielindamaldonado@gmail.com>
2022-04-18 17:54:02 -0400

    removed commented code that was needed and fixed hourly rate and total earnings to display.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,9 @@
 // DOM ELEMENTS
 var addProjectBtn = $('#add-project-btn');
-var deleteBtn = $('#delete-btn');
+var daysLeft = $('#days-left');
+var totalEarnings = $('#total-earnings');
 
 // VARIABLES
-var i = 0;
 
 
 
@@ -27,12 +27,11 @@ $( function() {
     var dialog, form,
     projectName = $( "#project-name" ),
     projectType = $( "#project-type" ),
-    rate = $( "#rate" ),
-    dueDate = $('#due-date'),
-    daysLeft = $('#days-left'),
-    totalEarnings = $('#total-earnings'),
-    allFields = $( [] ).add( projectName ).add( projectType ).add( rate ).add ( dueDate ).add( daysLeft ).add ( totalEarnings ),
-    tips = $( ".validateTips" );
+    rate = $( "#hourly-rate" ),
+    dueDate = $('#due-date');
+    // daysLeft = $('#days-left'),
+    // totalEarnings = $('#total-earnings'),
+    // allFields = $( [] ).add( projectName ).add( projectType ).add( rate ).add ( dueDate ).add( daysLeft ).add ( totalEarnings );
 
     $( "#due-date" ).datepicker();
     
@@ -49,63 +48,22 @@ $( function() {
         },
         close: function() {
           form[ 0 ].reset();
-          allFields.removeClass( "ui-state-error" );
         }
       });
 
-    function updateTips( t ) {
-      tips
-        .text( t )
-        .addClass( "ui-state-highlight" );
-      setTimeout(function() {
-        tips.removeClass( "ui-state-highlight", 1500 );
-      }, 500 );
-    }
- 
-    // function checkLength( o, n, min, max ) {
-    //   if ( o.val().length > max || o.val().length < min ) {
-    //     o.addClass( "ui-state-error" );
-    //     updateTips( "Length of " + n + " must be between " +
-    //       min + " and " + max + "." );
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // }
- 
-    // function checkRegexp( o, regexp, n ) {
-    //   if ( !( regexp.test( o.val() ) ) ) {
-    //     o.addClass( "ui-state-error" );
-    //     updateTips( n );
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // }
-  
-
-
-
-
+//   USER ENTRY INPUT --------------------------------------------------------------
   function addEntry() {
-    var valid = true;
-    allFields.removeClass( "ui-state-error" );
-    // valid = valid && checkLength( name, "username", 3, 16 );
-    // valid = valid && checkLength( email, "email", 6, 80 );
-    // valid = valid && checkLength( password, "password", 5, 16 );
-    
-    // valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-    // valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-    // valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-    
-    if ( valid ) {
+    totalEarnings = parseInt(rate.val()) * 8;
+    console.log(totalEarnings);
+
+    if (projectName.val() != "" && rate.val() != "" && dueDate.val() != "" && projectType.val() != null) {
         $( "#user-input" ).append( "<tr>" +
         "<td>" + projectName.val() + "</td>" +
         "<td>" + projectType.val() + "</td>" +
         "<td>" + rate.val() + "</td>" +
         "<td>" + dueDate.val() + "</td>" +
-        "<td>" + daysLeft.val() + "</td>" +
-        "<td>" + totalEarnings.val() + "</td>" +
+        "<td>" + daysLeft + "</td>" +
+        "<td>" + totalEarnings + "</td>" +
         "<td>" + "<button class='delete-btn'>" + "x" + "</button>" + "</td>" +
         "</tr>" );
         dialog.dialog( "close" );
@@ -114,7 +72,7 @@ $( function() {
             console.log(this);
         })
     }
-    return valid;
+    return;
 }
 
 
@@ -124,8 +82,5 @@ $( function() {
     event.preventDefault();
     addEntry();
   });
-
-//   $( "#create-user" ).button().on( "click", function() {
-//     dialog.dialog( "open" );
   });
   
